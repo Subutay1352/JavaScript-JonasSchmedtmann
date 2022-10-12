b0445e74151810afe058abb9d7d113a0d5114dc3
Subutay1352 <90280598+Subutay1352@users.noreply.github.com>
2022-10-12 22:17:35 +0300

    well

--- guess_My_Number/script.js
+++ guess_My_Number/script.js
@@ -1,11 +1,58 @@
 'use strict';
 
+let secretNumber = Math.trunc(Math.random() * 20)+1;
+let score=20;
+let highScore =0;
 
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function() {
 const guess = Number(document.querySelector('.guess').value);
 if (!guess)
 {
-    document.querySelector('.message').textContent = '⛔ Please enter a guess!';
+    displayMessage('⛔ Please enter a guess!');
 }
+else if(guess===secretNumber && score>0)
+{
+    displayMessage('Correct number!');
+    if(score>highScore)
+    {
+        highScore =score;
+        document.querySelector('.highscore').textContent = highScore;    
+    }
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width='30rem';
+}
+else if(guess>secretNumber  && score>0)
+{
+    displayMessage('Too High!');
+    score--;
+    document.querySelector('.score').textContent =score;
+}
+else if(guess<secretNumber  && score>0)
+{
+    displayMessage('Too Low!');
+    score--;
+    document.querySelector('.score').textContent =score;
+}
+else
+{
+    displayMessage('Game Over!');
+}
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+
+    score=20;
+    secretNumber = Math.trunc(Math.random() * 20)+1;
+
+    displayMessage('Start guessing...!');
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width='15rem';
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
 });
